6dd84f6702c2776119ed261d8567678c2ff64112
AhmedAshraf-OdooConsultant <ahmed@akshab.sa>
2026-08-31 08:29:23 +0300

    Delete addons/akshab_stock_report directory

--- addons/akshab_stock_report/report/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import stock_report_engine
-from . import stock_report_pdf
-from . import xlsx_base
-from . import xlsx_mirror
-from . import stock_report_xlsx
-from . import stock_aging_xlsx
-from . import stock_turnover_xlsx
-from . import stock_status_xlsx
